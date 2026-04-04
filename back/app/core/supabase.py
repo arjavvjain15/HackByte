dee4688ef2312914c717c43c2ebd4cf5df6ed4fe
Himanshu Yadav <162597444+himanshuuyadav@users.noreply.github.com>
2026-04-04 19:43:12 +0530

    Delete back directory

--- back/app/core/supabase.py
+++
@@ -1,8 +0,0 @@
-from supabase import create_client
-from app.core.config import get_env
-
-
-def get_supabase_client():
-    url = get_env("SUPABASE_URL")
-    key = get_env("SUPABASE_SERVICE_KEY")
-    return create_client(url, key)
